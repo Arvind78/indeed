39356672cb6af07318e0a2e598018fe6c0d6f48a
Arvind78 <93394422+Arvind78@users.noreply.github.com>
2022-11-11 15:26:53 +0530

    3 files modified, 2 added, 2 deleted

---
+++ src/componet/Swiper.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+// import { Swiper, SwiperSlide } from 'swiper/react';
+// import 'swiper/css';
+const Swiper =()=>{
+  return(
+    <>
+     <h2>hello</h2>
+ 
+    </>
+  )
+}
+export default Swiper;
